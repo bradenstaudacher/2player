56705f65eb9f68a383c283337c26fbd5ee66571b
bradenstaudacher <braden.staudacher@gmail.com>
2015-03-06 22:40:25 +0000

    Wrapped in Game class

--- game.rb
+++ game.rb
@@ -6,46 +6,38 @@ require "pry"
 #   attr_accessor(:players, :turn, :ui)
 #   def initialize
 #     @players = initplayers
-#     @turn = 1
-#     @ui = initui
+#     @turn = turn
 #   end
 # end
 
-#Game.new()
+# Game.new()
 
-
-
-
-
-def maintask
+class Game
   
-  loop do
-
-    turn = 1
-    @num1 = rollnum[0]
-    @num2 = rollnum[1]
-    askquestion(@player1, @num1, @num2)
-    answerhandler(@player1, @num1, @num2)
-    break if gameover?
-
-    turn = 2
-    @num1 = rollnum[0]
-    @num2 = rollnum[1]
-    askquestion(@player2, @num1, @num2)
-    answerhandler(@player2, @num1, @num2)
-    break if gameover?
-
+  def start
+    puts "Please enter your name Player1"
+    @player1 = Player.new(gets.chomp,0,3)
+    puts "Please enter your name Player2"
+    @player2 = Player.new(gets.chomp,0,3)
+    maintask
   end
+
+  def turn(player)
+    num1 = rollnum[0]
+    num2 = rollnum[1]
+    askquestion(player, num1, num2)
+    answerhandler(player, num1, num2)
+    gameover?
+  end
+
+  def maintask
+    loop do
+      break if turn(@player1)
+      break if turn(@player2)
+    end
+  end
+
+  
 end
 
-
-def initplayers
-  puts "Please enter your name Player1"
-  @player1 = Player.new(gets.chomp,0,3)
-  puts "Please enter your name Player2"
-  @player2 = Player.new(gets.chomp,0,3)
-  return [@player1, @player2]
-end
-
-initplayers
-maintask
+Game.new().start
